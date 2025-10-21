72f0484abd320ad87e9f66f49f3ed814abd518c3
Ode Malfait <ode.malfait@student.ucll.be>
2025-10-21 15:56:32 +0200

    some more styling to homepage

--- petfolio/App.js
+++ petfolio/App.js
@@ -4,12 +4,16 @@ import { StyleSheet, Text, View, Image } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image
-        source={require("./assets/petfolio-logo.png")}
-        style={styles.image}
-      />
-      <Text>Welcome To</Text>
-      <Text>PetFolio</Text>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("./assets/petfolio-logo.png")}
+          style={styles.image}
+        />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Welcome To</Text>
+        <Text style={styles.text}>PetFolio</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,6 +25,18 @@ const styles = StyleSheet.create({
     backgroundColor: "#F6F1EB",
     alignItems: "center",
     justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: 100,
+  },
+  textContainer: {
+    flex: 2,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
   },
   text: {
     color: "#3D3D3D",
