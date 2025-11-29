805865f7912e92bdd17f54d9cdea37c23720a795
Ode Malfait <ode.malfait@student.ucll.be>
2025-11-29 15:16:56 +0100

    had to restart supabase

--- petfolio/utils/supabase.ts
+++ petfolio/utils/supabase.ts
@@ -1,10 +1,10 @@
-import 'react-native-url-polyfill/auto'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { createClient, processLock } from '@supabase/supabase-js'
+import "react-native-url-polyfill/auto";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient, processLock } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.EXPO_PUBLIC_SUPABASE_URL!,
-  process.env.EXPO_PUBLIC_SUPABASE_KEY!,
+  process.env.EXPO_PUBLIC_SUPABASE_URL,
+  process.env.EXPO_PUBLIC_SUPABASE_KEY,
   {
     auth: {
       storage: AsyncStorage,
@@ -13,4 +13,5 @@ export const supabase = createClient(
       detectSessionInUrl: false,
       lock: processLock,
     },
-  })
+  }
+);
