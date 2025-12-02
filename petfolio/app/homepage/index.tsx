71984e8435f36455cacc385ecdc2b4eaa15e71c6
Ode Malfait <ode.malfait@student.ucll.be>
2025-12-02 09:22:07 +0100

    refined the logout and redirect to index page when not logged in

--- petfolio/app/homepage/index.tsx
+++ petfolio/app/homepage/index.tsx
@@ -1,34 +1,58 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, ActivityIndicator } from "react-native";
 import Button from "../../components/Button";
 import Header from "../../components/Header";
 import { useRouter } from "expo-router";
 import { useAuth } from "../../context/authContext";
+import { useEffect, useState } from "react";
 
 export default function Homepage() {
   const router = useRouter();
   const { user, loading: authLoading } = useAuth();
+  const [loading, setLoading] = useState(true);
 
-  if (user) return  (
- <View style={styles.container}>
-      <Header />
-      <View style={styles.imageContainer}>
-        <Image
-          source={require("../../assets/petfolio-logo.png")}
-          style={styles.image}
-        />
-        <View>
-          <Text style={styles.title}>Welcome To Petfolio</Text>
+  useEffect(() => {
+    if (!user) {
+      setLoading(true);
+    } else {
+      setLoading(false);
+    }
+  }, [user]);
+
+  // Add loading state
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="rgba(0, 28, 5, 1)" />
+          <Text>Loading data...</Text>
         </View>
       </View>
-      <View style={styles.buttonContainer}>
-        <Button
-          label="Pet Overview"
-          onPress={() => router.navigate("/petOverview")}
-        />
-        <Button label="Add Pet" onPress={() => router.navigate("/addPet")} />
+    );
+  }
+
+  if (user)
+    return (
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.imageContainer}>
+          <Image
+            source={require("../../assets/petfolio-logo.png")}
+            style={styles.image}
+          />
+          <View>
+            <Text style={styles.title}>Welcome To Petfolio</Text>
+          </View>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            label="Pet Overview"
+            onPress={() => router.navigate("/petOverview")}
+          />
+          <Button label="Add Pet" onPress={() => router.navigate("/addPet")} />
+        </View>
       </View>
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
@@ -38,6 +62,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginBottom: 0,
     maxWidth: "100%",
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
   },
   imageContainer: {
     alignItems: "center",
