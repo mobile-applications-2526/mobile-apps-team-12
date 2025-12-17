f7b4f00b87b7e5e239ed18e446d4df015e6cf8f6
Ode Malfait <ode.malfait@student.ucll.be>
2025-12-17 16:08:54 +0100

    changed anon-user image name

--- petfolio/components/profile/ProfileOverview.tsx
+++ petfolio/components/profile/ProfileOverview.tsx
@@ -107,7 +107,7 @@ export default function ProfileOverview({ profileData }: Props) {
           source={
             profileImageUrl
               ? { uri: profileImageUrl }
-              : require("../../assets/anon_user.png")
+              : require("../../assets/anon-user.png")
           }
           style={styles.profilePic}
         />
